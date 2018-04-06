a501a94a54670a80af35939dd693b7ef20a2cbdc
takahiro-tsuruda <takahiro-tsuruda@cybozu.co.jp>
2018-04-06 18:15:39 +0900

    POST /api/v1/crypts/{path}

--- crypts.go
+++ crypts.go
@@ -1,11 +1,15 @@
 package sabakan
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 
+	"io/ioutil"
+
 	"github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/clientv3/concurrency"
 	"github.com/gorilla/mux"
 )
 
@@ -20,7 +24,7 @@ type etcdClient struct {
 
 func (e *etcdClient) initCryptsFunc(r *mux.Router) {
 	r.HandleFunc("/{serial}/{path}", e.handleCryptsGet).Methods("GET")
-	//r.HandleFunc("/{serial}/", e.handleCryptsPost).Methods("POST")
+	r.HandleFunc("/{serial}", e.handleCryptsPost).Methods("POST")
 }
 
 func InitCrypts(r *mux.Router, c *clientv3.Client) {
@@ -30,37 +34,116 @@ func InitCrypts(r *mux.Router, c *clientv3.Client) {
 
 func (e *etcdClient) handleCryptsGet(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
+	serial := vars["serial"]
+	path := vars["path"]
+
 	resp, err := e.c.Get(r.Context(),
-		fmt.Sprintf("/crypts/%v/%v", vars["serial"], vars["path"]))
+		fmt.Sprintf("/crypts/%v/%v", serial, path))
+	if err != nil {
+		w.Write([]byte(err.Error() + "\n"))
+		return
+	}
+	if len(resp.Kvs) == 0 {
+		w.WriteHeader(404)
+		return
+	}
+	if len(resp.Kvs) != 1 {
+		w.WriteHeader(500)
+		return
+	}
+
+	ev := resp.Kvs[0]
+	entity := &cryptEntity{
+		Path: string(path),
+		Key:  string(ev.Value)}
+	res, err := json.Marshal(entity)
 	if err != nil {
 		w.Write([]byte(err.Error() + "\n"))
 		return
 	}
 
-	if len(resp.Kvs) == 0 {
-		w.WriteHeader(404)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
+}
+
+func (e *etcdClient) handleCryptsPost(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	serial := vars["serial"]
+	var receivedEntity cryptEntity
+	b, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(b, &receivedEntity)
+	path := receivedEntity.Path
+	key := receivedEntity.Key
+
+	s, err := concurrency.NewSession(e.c)
+	defer s.Close()
+	if err != nil {
+		w.Write([]byte(err.Error() + "\n"))
+		return
+	}
+	m := concurrency.NewMutex(s, "/sabakan-post-crypts-lock/")
+	if err := m.Lock(context.TODO()); err != nil {
+		w.Write([]byte(err.Error() + "\n"))
 		return
 	}
 
-	for _, ev := range resp.Kvs {
-		entity := &cryptEntity{
-			Path: string(vars["path"]),
-			Key:  string(ev.Value)}
-
-		res, err := json.Marshal(entity)
-		if err != nil {
-			w.Write([]byte(err.Error() + "\n"))
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(res)
+	// Prohibit overwriting
+	check, err := e.c.Get(r.Context(),
+		fmt.Sprintf("/crypts/%v/%v", serial, path))
+	if err != nil {
+		w.Write([]byte(err.Error() + "\n"))
+		return
 	}
-}
+	if len(check.Kvs) == 1 {
+		w.WriteHeader(400)
+		return
+	}
 
-//func (e *etcdClient) handleCryptsPost(w http.ResponseWriter, r *http.Request) {
-//	vars := mux.Vars(r)
-//	resp, err := e.c.Put(r.Context(),
-//		fmt.Sprintf("/crypts/%v/%v", vars["serial"], vars["path"]))
-//
-//}
+	// Put crypts on etcd
+	resp, err := e.c.Put(r.Context(),
+		fmt.Sprintf("/crypts/%v/%v", serial, path),
+		key)
+	if err != nil && resp != nil {
+		w.Write([]byte(err.Error() + "\n"))
+		return
+	}
+
+	// Confirm whether it was saved in etcd
+	check, err = e.c.Get(r.Context(),
+		fmt.Sprintf("/crypts/%v/%v", serial, path))
+	if err != nil {
+		w.Write([]byte(err.Error() + "\n"))
+		return
+	}
+	if len(check.Kvs) == 0 {
+		w.WriteHeader(404)
+		return
+	}
+	if len(check.Kvs) != 1 {
+		w.WriteHeader(500)
+		return
+	}
+	ev := check.Kvs[0]
+	savedKey := string(ev.Value)
+	if savedKey != key {
+		w.WriteHeader(500)
+		return
+	}
+
+	if err := m.Unlock(context.TODO()); err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	responseEntity := &cryptEntity{
+		Path: string(path),
+		Key:  string(savedKey)}
+	res, err := json.Marshal(responseEntity)
+	if err != nil {
+		w.Write([]byte(err.Error() + "\n"))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(201)
+	w.Write(res)
+}
