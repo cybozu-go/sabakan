e71ac63320684fbdb071425ce963366f29a49a1c
takahiro-tsuruda <takahiro-tsuruda@cybozu.co.jp>
2018-04-10 15:09:41 +0900

    Add unit test for crypts

---
+++ crypts_test.go
@@ -0,0 +1,27 @@
+package sabakan
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_respWritr(t *testing.T) {
+	w := httptest.NewRecorder()
+	entity := cryptEntity{Path: "path", Key: "aaa"}
+	respWriter(w, entity, http.StatusCreated)
+	resp := w.Result()
+	var sut cryptEntity
+	json.NewDecoder(resp.Body).Decode(&sut)
+
+	if resp.StatusCode != http.StatusCreated {
+		t.Fatal("expected 201. actual: ", resp.StatusCode)
+	}
+	if resp.Header.Get("Content-Type") != "application/json" {
+		t.Fatal("expected application/json")
+	}
+	if sut == entity {
+		t.Fatal("invalid response body")
+	}
+}
