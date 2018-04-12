199e474204697e96edbcb22b46bb0fce21d50120
takahiro-tsuruda <takahiro-tsuruda@cybozu.co.jp>
2018-04-12 14:28:42 +0900

    Also output log on success

--- responses.go
+++ responses.go
@@ -4,13 +4,22 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"fmt"
+
+	"strings"
+
+	"strconv"
+
 	"github.com/cybozu-go/log"
 )
 
 func respWriter(w http.ResponseWriter, data interface{}, status int) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	err := json.NewEncoder(w).Encode(data)
+	out, err := json.Marshal(data)
+	outstr := string(out)
+	log.Info(strings.Join([]string{"status:", strconv.Itoa(status), ", response_body:", outstr}, " "), nil)
+	fmt.Fprintf(w, outstr)
 	if err != nil {
 		return err
 	}
