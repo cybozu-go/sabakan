f12e45fc34d9c8e6566277df2141b4fa76beaef3
mitz <mitz@cybozu.co.jp>
2018-05-23 08:55:50 +0000

    [dhcpd] Implement discover_test.go

--- dhcpd/discover_test.go
+++ dhcpd/discover_test.go
@@ -71,6 +71,14 @@ func testComparePacket(t *testing.T, resp, expected *dhcp4.Packet) {
 	testIPEqual(t, "ServerAddr", resp.ServerAddr, expected.ServerAddr)
 	testIPEqual(t, "RelayAddr", resp.RelayAddr, expected.RelayAddr)
 
+	if resp.BootServerName != expected.BootServerName {
+		t.Error(`wrong resp.BootServerName:`, resp.BootServerName, expected.BootServerName)
+	}
+
+	if resp.BootFilename != expected.BootFilename {
+		t.Error(`wrong resp.BootFilename:`, resp.BootFilename, expected.BootFilename)
+	}
+
 	for k, v := range expected.Options {
 		v2, ok := resp.Options[k]
 		if !ok {
@@ -123,14 +131,89 @@ func testDiscoverDirect(t *testing.T) {
 
 func testDiscoverRelayed(t *testing.T) {
 	t.Parallel()
+
+	h := testNewHandler(26, 1, 0)
+
+	pkt := testDiscoverPacket()
+	pkt.RelayAddr = []byte{10, 69, 0, 129}
+	intf := testDiscoverInterface()
+	expected := testDiscoverPacket()
+	expected.Type = dhcp4.MsgOffer
+	expected.YourAddr = []byte{10, 69, 0, 160}
+	expected.ServerAddr = []byte{10, 69, 1, 3}
+	expected.BootServerName = "10.69.1.3"
+	expected.RelayAddr = []byte{10, 69, 0, 129}
+	expected.Options[dhcp4.OptSubnetMask] = []byte{255, 255, 255, 192}
+	expected.Options[dhcp4.OptRouters] = []byte{10, 69, 0, 129}
+	buf := make([]byte, 4)
+	binary.BigEndian.PutUint32(buf, 3600)
+	expected.Options[dhcp4.OptLeaseTime] = buf
+	expected.Options[dhcp4.OptServerIdentifier] = []byte{10, 69, 1, 3}
+
+	resp, err := h.handleDiscover(context.Background(), pkt, intf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	testComparePacket(t, resp, expected)
 }
 
 func testDiscoverHTTPBoot(t *testing.T) {
 	t.Parallel()
+
+	h := testNewHandler(26, 1, 0)
+
+	pkt := testDiscoverPacket()
+	pkt.Options[93] = []byte{0x00, 0x10}
+	pkt.Options[dhcp4.OptVendorIdentifier] = []byte("HTTPClient:hogehoge")
+	intf := testDiscoverInterface()
+	expected := testDiscoverPacket()
+	expected.Type = dhcp4.MsgOffer
+	expected.YourAddr = []byte{10, 69, 1, 32}
+	expected.ServerAddr = []byte{10, 69, 1, 3}
+	expected.BootServerName = "10.69.1.3"
+	expected.Options[dhcp4.OptSubnetMask] = []byte{255, 255, 255, 192}
+	expected.Options[dhcp4.OptRouters] = []byte{10, 69, 1, 1}
+	buf := make([]byte, 4)
+	binary.BigEndian.PutUint32(buf, 3600)
+	expected.Options[dhcp4.OptLeaseTime] = buf
+	expected.Options[dhcp4.OptServerIdentifier] = []byte{10, 69, 1, 3}
+	expected.Options[dhcp4.OptVendorIdentifier] = []byte("HTTPClient")
+	expected.BootFilename = "http://10.69.1.3:80/api/v1/boot/ipxe.efi"
+
+	resp, err := h.handleDiscover(context.Background(), pkt, intf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	testComparePacket(t, resp, expected)
 }
 
 func testDiscoverIPXE(t *testing.T) {
 	t.Parallel()
+
+	h := testNewHandler(26, 1, 0)
+
+	pkt := testDiscoverPacket()
+	pkt.Options[77] = []byte("iPXE")
+	intf := testDiscoverInterface()
+	expected := testDiscoverPacket()
+	expected.Type = dhcp4.MsgOffer
+	expected.YourAddr = []byte{10, 69, 1, 32}
+	expected.ServerAddr = []byte{10, 69, 1, 3}
+	expected.BootServerName = "10.69.1.3"
+	expected.Options[dhcp4.OptSubnetMask] = []byte{255, 255, 255, 192}
+	expected.Options[dhcp4.OptRouters] = []byte{10, 69, 1, 1}
+	buf := make([]byte, 4)
+	binary.BigEndian.PutUint32(buf, 3600)
+	expected.Options[dhcp4.OptLeaseTime] = buf
+	expected.Options[dhcp4.OptServerIdentifier] = []byte{10, 69, 1, 3}
+	expected.BootFilename = "http://10.69.1.3:80/api/v1/boot/coreos/ipxe"
+
+	resp, err := h.handleDiscover(context.Background(), pkt, intf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	testComparePacket(t, resp, expected)
+
 }
 
 func TestDiscover(t *testing.T) {
