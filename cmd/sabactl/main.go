3ed64777005cdae4c6ddecbcabcb9c32f63b8f65
Shin'ya UEOKA <ueokande@i-beam.org>
2018-06-05 23:11:48 +0000

    Discard logs from http client
    The errors are shown on failure, the logs are needed.

--- cmd/sabactl/main.go
+++ cmd/sabactl/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"flag"
+	"io/ioutil"
 	"net/http"
 	"os"
 
 	"github.com/cybozu-go/cmd"
+	"github.com/cybozu-go/log"
 	"github.com/cybozu-go/sabakan/client"
 	"github.com/google/subcommands"
 )
@@ -15,12 +17,20 @@ var (
 	flagServer = flag.String("server", "http://localhost:10080", "<Listen IP>:<Port number>")
 )
 
+var discardLogger *log.Logger
+
+func init() {
+	discardLogger = log.NewLogger()
+	discardLogger.SetOutput(ioutil.Discard)
+}
+
 func main() {
 	flag.Parse()
-	cmd.LogConfig{}.Apply()
 
 	c := client.NewClient(*flagServer, &cmd.HTTPClient{
-		Client: &http.Client{},
+		Severity: log.LvDebug,
+		Client:   &http.Client{},
+		Logger:   discardLogger,
 	})
 
 	subcommands.Register(subcommands.HelpCommand(), "")
