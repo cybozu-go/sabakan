50e96669b62f4068af8994809c58531490304b3b
Shin'ya UEOKA <ueokande@i-beam.org>
2018-06-08 00:08:04 +0000

    Fix newTestServer

--- web/main_test.go
+++ web/main_test.go
@@ -7,7 +7,9 @@ import (
 )
 
 func newTestServer(m sabakan.Model) *Server {
-	_, ipnet, err := net.ParseCIDR("192.0.2.1/32")
+	// httptest.NewRequest() sets RemoteAddr as "192.0.2.1:1234"
+	// https://golang.org/src/net/http/httptest/httptest.go?s=1162:1230#L31
+	_, ipnet, err := net.ParseCIDR("192.0.2.1/24")
 	if err != nil {
 		panic(err)
 	}
