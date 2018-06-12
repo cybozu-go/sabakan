c10f2bc5657a7c78f849cac3dd4172cb2821e363
ymmt <ymmt@cybozu.com>
2018-06-12 17:05:31 +0900

    Fix wrong etcd schema paths

--- models/etcd/constants.go
+++ models/etcd/constants.go
@@ -3,8 +3,8 @@ package etcd
 // Internal schema keys.
 const (
 	KeyCrypts      = "crypts/"
-	KeyDHCP        = "dhcp/"
-	KeyIPAM        = "ipam/"
+	KeyDHCP        = "dhcp"
+	KeyIPAM        = "ipam"
 	KeyLeaseUsages = "lease-usages/"
 	KeyMachines    = "machines/"
 	KeyNodeIndices = "node-indices/"
