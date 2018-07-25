19bfa8afa5d57f99ff9ce67ed9e851aecbfc9ebd
ymmt <ymmt@cybozu.com>
2018-07-25 18:20:34 +0900

    [itest] annotate asset test.

--- itest/assets_test.go
+++ itest/assets_test.go
@@ -10,12 +10,14 @@ import (
 
 var _ = Describe("assets", func() {
 
-	It("should be distributed between servers", func() {
+	It("should work as expected", func() {
 		f := localTempFile("test")
 		defer os.Remove(f.Name())
 
+		By("Uploading an asset")
 		sabactl("assets", "upload", "test", f.Name())
 
+		By("Checking all servers pull the asset")
 		Eventually(func() bool {
 			var info struct {
 				Urls []string `json:"urls"`
@@ -29,11 +31,18 @@ var _ = Describe("assets", func() {
 			return len(info.Urls) == 3
 		}).Should(BeTrue())
 
+		By("Removing the asset from the index")
 		sabactl("assets", "delete", "test")
-		for _, h := range []string{host1, host2, host3} {
-			Eventually(func() string {
-				return execSafeAt(h, "ls", "/var/lib/sabakan/assets")
-			}).Should(BeEmpty())
-		}
+
+		By("Checking all servers remove the asset")
+		Eventually(func() bool {
+			for _, h := range []string{host1, host2, host3} {
+				stdout, _, err := execAt(h, "ls", "/var/lib/sabakan/assets")
+				if err != nil || len(stdout) == 0 {
+					return false
+				}
+			}
+			return true
+		}).Should(BeTrue())
 	})
 })
